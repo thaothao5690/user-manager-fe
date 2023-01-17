bcd181e0759cddfd464b54cf01bd1f6fc5230152
jane <jane.tran@beyondedge.co>
2023-01-17 14:10:40 +0700

    [UI] Create login page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// function emitCommit(id) {
+//   setInterval(() => {
+//     window.dispatchEvent(new CustomEvent(`lesson-${id}`, {
+//       detail: `Comment of lesson ${id}`
+//     }))
+//   }, 2000)
+// }
+
+// emitCommit(1)
+// emitCommit(2)
+// emitCommit(3)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
