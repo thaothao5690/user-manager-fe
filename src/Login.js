5b9ab46279930f5604a997c72aa2316679f52267
victornguyen0412 <victor.nguyen@beyondedge.co>
2023-01-19 08:31:38 +0700

    Add generate token

--- src/Login.js
+++ src/Login.js
@@ -1,89 +1,121 @@
-import { Button, Form, Input, Checkbox, Row, Col } from 'antd';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { useEffect, useState } from 'react';
-import "./Login.css"
+import { Button, Form, Input, Checkbox, Row, Col } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import "./Login.css";
 
 function Login() {
-    const initValue = {
-        email: '',
-        password: '',
-    }
+  const initValue = {
+    email: "",
+    password: "",
+  };
 
-    const [data, setData] = useState(initValue)
+  const generateToken = (email) => {
+    const HMACSHA256 = (stringToSign, secret) => "not_implemented";
 
-    useEffect(() => {
-
-    }, [])
-
-    const onFinish = (values) => {
-        console.log('Success:', values);
-        setData(values)
-        localStorage.setItem('email', values.email)
+    // The header typically consists of two parts:
+    // the type of the token, which is JWT, and the signing algorithm being used,
+    // such as HMAC SHA256 or RSA.
+    const header = {
+      alg: "HS256",
+      typ: "JWT",
+      kid: "vpaas-magic-cookie-1fc542a3e4414a44b2611668195e2bfe/4f4910",
     };
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
+    const encodedHeaders = btoa(JSON.stringify(header));
+
+    // The second part of the token is the payload, which contains the claims.
+    // Claims are statements about an entity (typically, the user) and
+    // additional data. There are three types of claims:
+    // registered, public, and private claims.
+    const claims = {
+      email: email,
     };
+    const encodedPlayload = btoa(JSON.stringify(claims));
 
-    return (
-        <div className="App-login">
-            <Row justify="center">
-                <Col span={8}>
-                    <h2>Login</h2>
-                    <Form
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                    >
-                        <Form.Item
-                            name="email"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your Username!',
-                                },
-                            ]}
-                        >
-                            <Input
-                                prefix={<UserOutlined className="site-form-item-icon" />}
-                                type="email"
-                                placeholder="Enter email..."
-                            />
-                        </Form.Item>
-                        <Form.Item
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: "Please input your password"
-                                }
-                            ]}
-                        >
-                            <Input.Password
-                                prefix={<LockOutlined
-                                    className="site-form-item-icon" />}
-                                placeholder="Enter password..."
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Form.Item valuePropName="checked" className="login-form-remember">
-                                <Checkbox>Remember me</Checkbox>
-                            </Form.Item>
+    // create the signature part you have to take the encoded header,
+    // the encoded payload, a secret, the algorithm specified in the header,
+    // and sign that.
+    const signature = HMACSHA256(
+      `${encodedHeaders}.${encodedPlayload}`,
+      "mysecret"
+    );
+    const encodedSignature = btoa(signature);
 
-                            <a className="login-form-forgot" href="#">
-                                Forgot password?
-                            </a>
-                        </Form.Item>
+    const jwt = `${encodedHeaders}.${encodedPlayload}.${encodedSignature}`;
+    localStorage.setItem("jwt", jwt);
+  };
 
-                        <Form.Item>
-                            <Button htmlType="submit" className="login-form-button">
-                                Log in
-                            </Button>
-                            Or <a href="#">register now!</a>
-                        </Form.Item>
-                    </Form>
-                </Col>
-            </Row>
-        </div >
-    )
+  const [data, setData] = useState(initValue);
+
+  useEffect(() => {}, []);
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    setData(values);
+    generateToken(values.email);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="App-login">
+      <Row justify="center">
+        <Col span={8}>
+          <h2>Login</h2>
+          <Form onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                type="email"
+                placeholder="Enter email..."
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password",
+                },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Enter password..."
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item
+                valuePropName="checked"
+                className="login-form-remember"
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="#">
+                Forgot password?
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="#">register now!</a>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
